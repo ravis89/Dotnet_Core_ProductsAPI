f61a648d3a4b69da5f359238e8fa3da36eeabe9e
Sharma, Ravi Ranjan <Ravi.R.Sharma@CenturyLink.com>
2020-05-29 20:42:24 +0530

    Add project files.

---
+++ ProductsAPI/Services/IProductsService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductsAPI.Services
+{
+    public interface IProductsService
+    {
+         List<Product> GetProducts();
+
+        Product AddProduct(Product productItem);
+
+        Product UpdateProduct(string id, Product productItem);
+
+        string DeleteProduct(string id);
+    }
+}
